8b0028e55cbf6331cd040c9d1519dff4e8068b9c
pchuan98 <haeer98@outlook.com>
2023-10-09 21:23:17 +0800

    feat: fix a bug

--- scripts/tools/link.sh
+++ scripts/tools/link.sh
@@ -6,12 +6,15 @@ DOTFIRE_DIR=/home/chuan/dotfiles
 CONFIG_DIR=/home/chuan/.config
 HOME=/home/chuan
 
-# link dotfiles
+mkdir -p $CONFIG_DIR
+
+# link folders
 ln -sf $DOTFIRE_DIR/alacritty $CONFIG_DIR
 ln -sf $DOTFIRE_DIR/hypr $CONFIG_DIR
-mkdir -p $CONFIG_DIR/lazygit && ln -sf $DOTFIRE_DIR/lazygit/config.yml $CONFIG_DIR/lazygit/config.yml
+ln -sf $DOTFIRE_DIR/lazygit $CONFIG_DIR
 ln -sf $DOTFIRE_DIR/waybar $CONFIG_DIR
 ln -sf $DOTFIRE_DIR/nvim $CONFIG_DIR
-ln -s $DOTFIRE_DIR/yazi $CONFIG_DIR
+ln -sf $DOTFIRE_DIR/yazi $CONFIG_DIR
 
+# link files
 ln -sf $DOTFIRE_DIR/zsh/zshrc $HOME/.zshrc
